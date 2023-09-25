e7aa7f26fbf2e84c0cf10a9b4225dd62d9560e1e
AndreaRyholt <anra0003rstud.kea.dk>
2023-09-25 13:11:32 +0200

    tilføjede variabler

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,11 @@
 public class Room {
+    private String name;
+    private String description;
+    private String north;
+    private String south;
+    private String east;
+    private String west;
+
+
+
 }
