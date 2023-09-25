deea2fe9bb8afb5cbb450596c46dc6634aa0888a
juch0001 <juch0001@stud.kea.dk>
2023-09-25 13:14:46 +0200

    commit

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,2 +1,17 @@
 public class Room {
+    private String navn;
+    private String beskrivelse;
+    private String nord;
+    private String syd;
+    private String øst;
+    private String vest;
+
+    public Room (String navn, String beskrivelse, String nord, String syd, String øst, String vest){
+        this.navn= navn;
+        this.beskrivelse =beskrivelse;
+        this.nord =nord;
+        this.syd=syd;
+        this.øst=øst;
+        this.vest=vest;
+    }
 }
