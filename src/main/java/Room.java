966b0608bce4c3851ab6242b3e4867249e922722
juch0001 <juch0001@stud.kea.dk>
2023-09-25 21:19:45 +0200

    get og set

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -2,11 +2,69 @@ public class Room {
     //Todo objekt og variabler
     private String name;
     private String description;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
+
 
 
     public Room (String name, String description){
         this.name= name;
         this.description =description;
 
+    }
+    //Todo get og set for navn
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name=name;
+    }
+
+
+    //Todo get og set for beskrivelse
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description){
+        this.description=description;
+    }
+
+
+    //Todo get og set for nord
+    public Room getNorth() {
+        return north;
+    }
+    public void setNorth(Room north){
+        this.north=north;
+    }
+
+
+    //Todo get og set for syd
+    public Room getSouth() {
+        return south;
+    }
+    public void setSouth(Room south){
+        this.south=south;
+    }
+
+
+    //Todo get og set for øst
+    public Room getEast() {
+        return east;
+    }
+    public void setEast(Room east){
+        this.east=east;
+    }
+
+
+
+    //Todo get og set for vest
+    public Room getWest() {
+        return west;
+    }
+    public void setWest(Room west){
+        this.west=west;
     }
 }
