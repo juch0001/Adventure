b971ffa33b059c89ee6ac8a106a6ef889cbb399f
AndreaRyholt <anra0003rstud.kea.dk>
2023-09-28 16:26:20 +0200

    Refactoring

---
+++ src/main/java/Player.java
@@ -0,0 +1,85 @@
+public class Player {
+    public static final String BLACK_BOLD = "\033[1;30m"; //Markeret sort (hvis man har hvid skærm)
+
+    private Room currentRoom;
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+    //TODO if bruger skriver east -> go east osv.
+    public String moveToRoom(String input) {
+        Room nextRoom = null;
+        if (input.equals("go east") && currentRoom.getEast() != null) {
+            nextRoom = currentRoom.getEast();
+        } else if (input.equals("go south") && currentRoom.getSouth() != null) {
+            nextRoom = currentRoom.getSouth();
+        } else if (input.equals("go west") && currentRoom.getWest() != null) {
+            nextRoom = currentRoom.getWest();
+        } else if (input.equals("go north") && currentRoom.getNorth() != null) {
+            nextRoom = currentRoom.getNorth();
+        }
+        if (nextRoom != null) {
+            return currentRoom.getDescription();
+        } else {
+            return null;
+        }
+    }
+
+    public void directions(String menu) {
+        String arrow = " "; //For at kunne tilføje pile retning
+
+        switch (menu) {
+            case "go north":
+                arrow = " ↑ ";
+                String northResult = moveToRoom("go north");
+                if (northResult != null) {
+                    System.out.println(BLACK_BOLD + "going north" + arrow + "\u001B[0m");
+                    System.out.println("You are now in " + currentRoom.getName());
+                    System.out.println(currentRoom.getDescription());
+                } else {
+                    System.out.println("This path is not available");
+                }
+                break;
+            case "go east":
+                arrow = " → ";
+                String eastResult = moveToRoom("go east");
+                if (eastResult != null) {
+                    System.out.println(BLACK_BOLD + "going east" + arrow + "\u001B[0m");
+                    System.out.println("You are now in " + currentRoom.getName());
+                    System.out.println(currentRoom.getDescription());
+                } else {
+                    System.out.println("This path is not available");
+                }
+                break;
+            case "go south":
+                arrow = " ↓ ";
+                String southResult = moveToRoom("go south");
+                if (southResult != null) {
+                    System.out.println(BLACK_BOLD + "going south" + arrow + "\u001B[0m");
+                    System.out.println("You are now in " + currentRoom.getName());
+                    System.out.println(currentRoom.getDescription());
+                } else {
+                    System.out.println("This path is not available");
+                }
+                break;
+            case "go west":
+                arrow = " ← ";
+                String westResult = moveToRoom("go west");
+                if (westResult != null) {
+                    System.out.println(BLACK_BOLD + "going west" + arrow + "\u001B[0m");
+                    System.out.println("You are now in " + currentRoom.getName());
+                    System.out.println(currentRoom.getDescription());
+                } else {
+                    System.out.println("This path is not available");
+                }
+                break;
+            default:
+                System.out.println("not avaliable");
+                break;
+        }
+    }
+}
