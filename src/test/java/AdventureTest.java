5fc463ec2019db157aa2d7ab22265daa3480f5cb
AndreaRyholt <anra0003@stud.kea.dk>
2023-09-28 16:39:16 +0200

    Fixed AdventureTest

--- src/test/java/AdventureTest.java
+++ src/test/java/AdventureTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdventureTest {
-/*
+
 @Test
 public void always_True() {
     assertTrue(1 == 1);
@@ -13,23 +13,23 @@ public void always_True() {
 public void find_Current_Room() {
     //Arrange
     String expected = "Room 1";
-    Map map = new Map();
+    Adventure adventure = new Adventure();
     //Act
-    map.buildMap();
+    adventure.buildMap();
     //Assert
-    assertEquals(expected, map.getCurrentRoom().getName());
+    assertEquals(expected, adventure.getCurrentRoom());
 }
 
 @Test
 public void move_From_Current_Position_TO_ROOM2() {
     //Arrange
     String expected = "Room 2";
-    Map map = new Map();
-    map.buildMap();
+    Adventure adventure = new Adventure();
+    adventure.buildMap();
     //Act
    // adventure.moveToRoom("Room 2");
     //Assert
-    assertEquals("Room 2", map.getCurrentRoom().getName());
+    assertEquals("Room 2", adventure.getCurrentRoom());
 }
-*/
+
 }
